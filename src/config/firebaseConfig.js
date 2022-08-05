16923fe67828244769538c8711f8dd4f86c4618a
Shahriar Ahmed Shovon <shovonshahriar01@gmail.com>
2022-08-05 15:21:58 +0600

    Fixed strick mode + firebase analy

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,6 +1,8 @@
+import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -16,5 +18,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const analytics = getAnalytics(app);
 
-export { app, auth, db };
+export { app, auth, db, analytics };
